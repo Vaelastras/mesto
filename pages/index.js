480812b956197afee28fd6a88e96c0941baf17ad
Alexey Belov <vaelastras@gmail.com>
2020-05-11 03:10:20 +0300

    фикс скрипта

--- pages/index.js
+++ pages/index.js
@@ -9,41 +9,32 @@ const currentJob = document.querySelector('.profile__job-description');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 
+// Функции
+
+function openPopup () {
+  popup.classList.add('popup_opened');
+  nameInput.value = currentName.textContent;
+  jobInput.value = currentJob.textContent;
+}
+
+
+function closePopup () {
+  popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+  currentName.textContent = nameInput.value;
+  currentJob.textContent = jobInput.value;
+  closePopup()
+}
+
 
 //Слушатели
 
-closeButton.addEventListener('click', openPopup);     //ищем клики по кнопке закрытия попапа и запускаем функцию-закрытие
-editButton.addEventListener('click', swithAuthor);  //ищем клики по кнопки редактирования профиля и запускаем функцию-смены автора
-checkContainer.addEventListener('submit', formSubmitHandler); // ищем событие отправки формы и запускаем фукнцию-отправки формы
-
-// Функции
-
-// открытие/закрытие попапа
-function openPopup () {
-  if (popup.classList.contains('popup_opened')) { // логика  если в переменной popup есть опенер -
-    popup.classList.remove('popup_opened');     // то удали опенер и попап пропадёт
-  } else {                                        // а если нет
-    popup.classList.add('popup_opened');        // то добавь
-  }
-}
-
-//получение дефолтных данных с форм попапа
-function swithAuthor () {
-  openPopup();                                    // открываем попап
-  nameInput.value = currentName.textContent;      // получить значение из поля имя и записать его текстом
-  jobInput.value = currentJob.textContent;        // получить значение из поля работая и записать его текстом
-}
-
-//смена автора в профиле страницы
-function formSubmitHandler(event) {
-  event.preventDefault();
-  currentName.textContent = nameInput.value;      // вписать значение имени из попапа в страницу
-  currentJob.textContent = jobInput.value;        // вписать значение работы из попапа в страницу
-  openPopup();
-}
-
-
-
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
+checkContainer.addEventListener('submit', formSubmitHandler);
 
 
 
