deb32c263630527bb11c010f13008cdb5c69398c
Alexey Belov <vaelastras@gmail.com>
2020-05-30 17:55:29 +0300

    bugfix: 5rd Notion - repair FKING scripT!

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,25 @@
 //переменные
-const popup = document.querySelector('.popup'); // ищем обычный попап (попап1)
-const popupPlace = document.querySelector('.popup-newplace'); // ищем попап новых мест (попап 2)
-const popupImage = document.querySelector('.popup-image'); //ищем попап открытия изображений (попап 3)
+const popupParent = document.querySelector('.popups'); // общая секция для всех попапов
+// const popup = document.querySelector('.popup');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');// ищем обычный попап (попап1)
+const popupPlace = document.querySelector('.popup_type_new-place'); // ищем попап новых мест (попап 2)
+const popupImage = document.querySelector('.popup_type_image'); //ищем попап открытия изображений (попап 3)
 
 const editProfileButton = document.querySelector('.profile__edit-button'); // ищем кнопку вызова попапа редактирования профиля
 const addPlaceButton = document.querySelector('.profile__add-button'); // ищем кнопку вызова попапа добавления нового места
 
-const checkPopupContainer = document.querySelector('.popup__container'); // опрередяем форму, откуда будем тянуть инпуты имени пользователя и его профессии (попап 1)
-const checkPlaceContainer = document.querySelector('.popup-newplace__container'); // определяем форму, откуда будем тянуть инпуты названия места и ссылку (попап2)
-
-const closePopupEditButton = document.querySelector('.popup__close'); // ищем кнопку закрытия (попап 1)
-const closePopupPlaceButton = document.querySelector('.popup-newplace__close'); // ищем кнопку закрытия (попап 2)
-const closePopupImageButton = document.querySelector('.popup-image__close'); // ищем кнопку закрытия (попап 3)
-
+// const checkContainer = document.querySelector('.popup__container'); // опрередяем форму, откуда будем тянуть инпуты имени пользователя и его профессии (попап 1)
+const checkPlaceContainer = document.querySelector('.popup__container_type_card'); // определяем форму, откуда будем тянуть инпуты названия места и ссылку (попап2)
+// const closePopupButton = document.querySelector('.popup__close'); // ищем кнопку закрытия попапов
 const currentName = document.querySelector('.profile__title'); // ищем текущее имя юзера на странице
 const currentJob = document.querySelector('.profile__job-description'); //ищем текущуюю профессию юзера на странице
 const nameInput = document.querySelector('.popup__input_type_name'); // ищем инпут имени (попап 1)
 const jobInput = document.querySelector('.popup__input_type_job'); //ищем инпут профессии (попап 1)
-
-const popupPlaceName = document.querySelector('.popup-newplace__input_type_name'); // ищем инпут названия места (попап 2)
-const popupPlaceUrl = document.querySelector('.popup-newplace__input_type_url'); // ищем инпут ссылки (попап 2)
-const popupImagePhotoUrl = document.querySelector('.popup-image__picture') //ищем картинку места (попап 3)
-const popupImageTitle = document.querySelector(".popup-image__title")// ищем название места (попап 3)
-
+const popupPlaceName = document.querySelector('.popup__input_type_title'); // ищем инпут названия места (попап 2)
+const popupPlaceUrl = document.querySelector('.popup__input_type_url'); // ищем инпут ссылки (попап 2)
 const elements = document.querySelector('.elements'); //определяем место где будут создаваться карточки
-
+const popupImagePhotoUrl = document.querySelector('.popup__image') //ищем картинку места (попап имг)
+const popupImageTitle = document.querySelector('.popup__image-title')// ищем название места (попап имг)
 
 /* определяем массив изначальных элементов. каждый элемент - объект с свойствами
 Свойства name и link - пригодятся как параметры функции при создании дефолтных карт
@@ -57,35 +52,55 @@ const initialCards = [
 ]
 const elementTemplate = document.querySelector('#template').content; // ищем шаблон темплейта для клонирования карточек
 
-
-
 // Функции
 
-function openCloseEditPopup () {
-  if (!popup.classList.contains('popup_opened')) { //если на элементе нет класса
-    popup.classList.add('popup_opened');           // то добавим
-    nameInput.value = currentName.textContent;     // а заодно и подставим в поле значение с страницы
-    jobInput.value = currentJob.textContent;       // а заодно и подставим в поле значение с страницы
-  } else {
-      popup.classList.remove('popup_opened');     // или удалим класс и попапчик скроется
+//открытие/закрытие попапов
+function openPopup(element) {
+  element.classList.toggle('popup_active');
+}
+
+// закрытие попапов кнопкой
+function closePopup(evt) {
+  if (evt.target.classList.contains('popup__close')) {
+    evt.target.closest('.popup').classList.toggle('popup_active');
   }
 }
 
+//получить текущее имя пользователя в инпуты
+function getProfileName() {
+  nameInput.value = currentName.textContent; // а заодно и подставим в поле значение с страницы
+  jobInput.value = currentJob.textContent;
+  openPopup(popupEditProfile);
+}
+
 //f получения данных в попап с страницы
-function formSubmitHandler(event) {
-  event.preventDefault();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
   currentName.textContent = nameInput.value;
   currentJob.textContent = jobInput.value;
-  openCloseEditPopup()
+  openPopup(popupEditProfile);
 }
 
-function openClosePlacePopup() {
-  popupPlace.classList.toggle('popup-newplace_opened');
+// функция постановки лаек
+function toggleLike(evt) {
+  evt.target.classList.toggle('element__like_active')
 }
 
-function openCloseImagePopup() {
-  popupImage.classList.toggle('popup-image_opened');
+//функция удаления карточек
+function removeCard(evt) {
+  evt.target.closest('.element').remove()
 }
+
+
+//отображаем карточку пользователя
+function renderUserCard(event) {
+  event.preventDefault();
+
+  renderCard(popupPlaceName.value, popupPlaceUrl.value);
+  popupPlaceName.value = ''; // обнуляем значение инпута
+  popupPlaceUrl.value =''; // обнуляем значение инпута
+  openPopup(popupPlace)
+};
 
 function renderCard(name, link) {
   const card = elementTemplate.cloneNode(true); // клонируем шаблон
@@ -97,52 +112,43 @@ function renderCard(name, link) {
   cardTitle.textContent = name; // присваиваем текст на параметр функции
   cardImage.src = link; // присваиваем сслку на параметр
   cardImage.setAttribute('alt', name); // устанавливаем аттрибут альт для картинки с названием нейма
+  likeButton.addEventListener('click', toggleLike);
+  removeButton.addEventListener('click', removeCard);
+  cardImage.addEventListener('click', showPictureinPopup);
 
-  //вешаем слушатель на лайк, при клике - меняем класс
-  likeButton.addEventListener('click', (event) => event.target.classList.toggle('element__like_active'));
-  //вешаем слушатель на кнопку удаления карточки, при клике - сносим всю карточку. Из массива пока что не удаляем.
-  removeButton.addEventListener('click', (event) => event.target.closest('.element').remove());
-
-// создадим увеличенное изображение в попапе
-  cardImage.addEventListener('click', function () {  //слушатель клика на фотографию
-    const popupImagePhotoUrl = document.querySelector('.popup-image__picture') //ищем картинку места (попап 3)
-    const popupImageTitle = document.querySelector('.popup-image__title')// ищем название места (попап 3)
-
-    popupImageTitle.textContent = name;  // берем название фото из объекта
-    popupImagePhotoUrl.src = link; // берем ссылку из объекта
-    popupImagePhotoUrl.setAttribute('alt', name); // установим альт
-
-    openCloseImagePopup()
-
-  });
-  return card;   // возвращаем карту с элементами слушателями и параметрами
+  pasteCardInDocument(card);   // возвращаем карту с элементами слушателями и параметрами
 }
+
 // добавляем каждую карту на страницу. данные берутся из массива initial.cards.
- //TODO:, сделать чтобы новые карты добавлялись в массив */
-initialCards.forEach(element => elements.append(renderCard(element.name, element.link)));
-
-
-//отображаем шаблон
-function renderTemplate(event) {
-  //отмена стандартного submit
-  event.preventDefault();
-  const element = renderCard(popupPlaceName.value, popupPlaceUrl.value); //вставляем ссылку и название карточки
-
-  elements.prepend(element); // добавляем элемент в начало секции
-
-  popupPlaceName.value = ''; // обнуляем значение инпута
-  popupPlaceUrl.value = ''; // обнуляем значение инпута
-
-  openClosePlacePopup()
+function loadingCards() {
+  initialCards.forEach(showTemplateCardOnPage)
 };
 
+//insert-card to page
+function pasteCardInDocument(element) {
+  elements.prepend(element)
+}
+
+
+function showTemplateCardOnPage(element) {
+  renderCard(element.name, element.link)
+};
+
+function showPictureinPopup(evt) {
+  openPopup(popupImage)
+  popupImageTitle.textContent = evt.target.closest('.element').textContent; //берем текст c ближайшего эла
+  console.log(evt.target)
+  popupImagePhotoUrl.src = evt.target.src; // берем ссылку из объекта
+  popupImagePhotoUrl.setAttribute('alt', name); // установим альт
+}
+
+loadingCards()
 
 //Слушатели
 
-editProfileButton.addEventListener('click', openCloseEditPopup);
-closePopupEditButton.addEventListener('click', openCloseEditPopup);
-checkPopupContainer.addEventListener('submit', formSubmitHandler);
-addPlaceButton.addEventListener('click', openClosePlacePopup);
-closePopupPlaceButton.addEventListener('click', openClosePlacePopup);
-checkPlaceContainer.addEventListener('submit', renderTemplate);
-closePopupImageButton.addEventListener('click', openCloseImagePopup);
+popupEditProfile.addEventListener('submit', formSubmitHandler);
+checkPlaceContainer.addEventListener('submit', renderUserCard);
+editProfileButton.addEventListener('click', getProfileName);
+popupParent.addEventListener('click', closePopup)
+addPlaceButton.addEventListener('click', () => openPopup(popupPlace));
+
